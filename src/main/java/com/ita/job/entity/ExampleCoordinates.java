d3cd4ecebbbef780d0be27cf33bcaa0fec667063
yzhao121 <yzhao142857@gmail.com>
2021-01-25 22:55:09 -0500

    v0.0.2
    Test:
    Cerate ExpamleBookServlet and ExampleJobServletfor testing  RESTful api
    
    Add two entity class ExampleJob and ExampleCoordinate for assisting the test
    
    Add libraries:
    Json, IO utility and
    Jackson for parsing Json
    
    Create Two Servlet:
    SearchServlet and RecommendationServlet

---
+++ src/main/java/com/ita/job/entity/ExampleCoordinates.java
@@ -0,0 +1,12 @@
+package com.ita.job.entity;
+
+public class ExampleCoordinates {
+    public double latitude;
+    public double longitude;
+
+    public ExampleCoordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+}
---
+++ src/main/java/com/ita/job/servlet/ExampleBookServlet.java
@@ -0,0 +1,65 @@
+package com.ita.job.servlet;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ExampleBookServlet", urlPatterns = {"/example_book"})
+public class ExampleBookServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String title = jsonRequest.getString("title");
+        String author = jsonRequest.getString("author");
+        String date = jsonRequest.getString("date");
+        float price = jsonRequest.getFloat("price");
+        String currency = jsonRequest.getString("currency");
+        int pages = jsonRequest.getInt("pages");
+        String series = jsonRequest.getString("series");
+        String language = jsonRequest.getString("language");
+        String isbn = jsonRequest.getString("isbn");
+
+        System.out.println("Title is: " + title);
+        System.out.println("Author is: " + author);
+        System.out.println("Date is: " + date);
+        System.out.println("Price is: " + price);
+        System.out.println("Currency is: " + currency);
+        System.out.println("Pages is: " + pages);
+        System.out.println("Series is: " + series);
+        System.out.println("Language is: " + language);
+        System.out.println("ISBN is: " + isbn);
+
+
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String keyword = request.getParameter("keyword");
+        String category = request.getParameter("category");
+
+        System.out.println("keyword is:" + keyword);
+        System.out.println("category is:" + category);
+
+        response.setContentType("application/json");
+        JSONObject json = new JSONObject();
+        json.put("title", "Harry Potter and the Sorcerer's Stone");
+        json.put("author", "JK Rowling");
+        json.put("date", "October 1, 1998");
+        json.put("price", 11.99);
+        json.put("currency", "USD");
+        json.put("pages", 309);
+        json.put("series", "Harry Potter");
+        json.put("language", "en_US");
+        json.put("isbn", "0590353403");
+        response.getWriter().print(json);
+
+    }
+}
