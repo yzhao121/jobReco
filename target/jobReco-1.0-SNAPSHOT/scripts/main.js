98063dba525f6c6d001755f3c55d9da04b8fc11b
yzhao121 <yzhao142857@gmail.com>
2021-02-18 00:27:16 -0500

    v0.1.1
    Frontend:
      Update main.js, add ajax part, bind nav buttons with eventListener.
      Update index.html, add a template to display nearby, favorites and recommendation.
    
    Test

--- target/jobReco-1.0-SNAPSHOT/scripts/main.js
+++ target/jobReco-1.0-SNAPSHOT/scripts/main.js
@@ -1,10 +1,9 @@
 //use IIFE to do scope isolation
-(function() {
+;(function () {
     // get all elements
     var oAvatar = document.getElementById('avatar'),
         oWelcomeMsg = document.getElementById('welcome-msg'),
         oLogoutBtn = document.getElementById('logout-link'),
-        oRegisterFormBtn = document.getElementById('register-form-btn'),
         oLoginBtn = document.getElementById('login-btn'),
         oLoginForm = document.getElementById('login-form'),
         oLoginUsername = document.getElementById('username'),
@@ -12,28 +11,34 @@
         oLoginFormBtn = document.getElementById('login-form-btn'),
         oLoginErrorField = document.getElementById('login-error'),
         oRegisterBtn = document.getElementById('register-btn'),
+        oRegisterForm = document.getElementById('register-form'),
         oRegisterUsername = document.getElementById('register-username'),
         oRegisterPwd = document.getElementById('register-password'),
         oRegisterFirstName = document.getElementById('register-first-name'),
         oRegisterLastName = document.getElementById('register-last-name'),
-        oRegisterForm = document.getElementById('register-form'),
+        oRegisterFormBtn = document.getElementById('register-form-btn'),
         oRegisterResultField = document.getElementById('register-result'),
         oNearbyBtn = document.getElementById('nearby-btn'),
+        oFavBtn = document.getElementById('fav-btn'),
         oRecommendBtn = document.getElementById('recommend-btn'),
+        oNavBtnBox = document.getElementsByClassName('main-nav')[0],
         oNavBtnList = document.getElementsByClassName('main-nav-btn'),
         oItemNav = document.getElementById('item-nav'),
         oItemList = document.getElementById('item-list'),
+        oTpl = document.getElementById('tpl').innerHTML,
+
+        // default data
         userId = '1111',
         userFullName = 'John',
-        lng = -122.08,
-        lat = 37.38;
+        // lng = -122.08,
+        // lat = 37.38,
+        lng = -122,
+        lat = 47,
+        itemArr;
 
-
-    // start fn
+    // init
     function init() {
-        // logic
         validateSession();
-        // bind event
         bindEvent();
     }
 
@@ -42,43 +47,123 @@
     }
 
     function bindEvent() {
-        // switch between login and register
-        oRegisterFormBtn.addEventListener('click', function(){
+        oRegisterFormBtn.addEventListener('click', function () {
             switchLoginRegister('register')
         }, false);
-        oLoginFormBtn.addEventListener('click', function() {
+        oLoginFormBtn.addEventListener('click', function () {
             switchLoginRegister('login')
         }, false);
-
-        // click login button
         oLoginBtn.addEventListener('click', loginExecutor, false);
-
-        // click register button
         oRegisterBtn.addEventListener('click', registerExecutor, false);
+        oNearbyBtn.addEventListener('click', loadNearbyData, false);
+        oFavBtn.addEventListener('click', loadFavoriteItems, false);
+        oRecommendBtn.addEventListener('click', loadRecommendedItems, false);
+        oItemList.addEventListener('click', changeFavoriteItem, false);
     }
 
+    function switchLoginRegister(name) {
+        showOrHideElement(oAvatar, 'none');
+        showOrHideElement(oWelcomeMsg, 'none');
+        showOrHideElement(oLogoutBtn, 'none');
+        showOrHideElement(oItemNav, 'none');
+        showOrHideElement(oItemList, 'none');
+
+        if (name === 'login') {
+            showOrHideElement(oRegisterForm, 'none');
+            oRegisterResultField.innerHTML = ''
+            showOrHideElement(oLoginForm, 'block');
+
+        } else {
+            showOrHideElement(oLoginForm, 'none');
+            oLoginErrorField.innerHTML = '';
+            showOrHideElement(oRegisterForm, 'block');
+        }
+    }
+
+    /**
+     * API Login
+     */
     function loginExecutor() {
-        console.log('login');
         var username = oLoginUsername.value,
             password = oLoginPwd.value;
 
-        if (username === "" || password === "") {
+        if (username === "" || password == "") {
             oLoginErrorField.innerHTML = 'Please fill in all fields';
             return;
         }
-        var encodedPwd = md5(username + md5(password));
-        console.log(username, encodedPwd);
+        password = md5(username + md5(password));
+
+        ajax({
+            method: 'POST',
+            url: './login',
+            data: {
+                user_id: username,
+                password: password,
+            },
+            success: function (res) {
+                if (res.status === 'OK') {
+                    welcomeMsg(res);
+                    fetchData();
+                } else {
+                    oLoginErrorField.innerHTML = 'Invalid username or password';
+                }
+            },
+            error: function () {
+                throw new Error('Invalid username or password');
+            }
+        })
     }
 
+    /**
+     * API Change Favorite Item
+     * @param evt
+     */
+    function changeFavoriteItem(evt) {
+        var tar = evt.target,
+            oParent = tar.parentElement;
+
+        if (oParent && oParent.className === 'fav-link') {
+            console.log('change ...')
+            var oCurLi = oParent.parentElement,
+                classname = tar.className,
+                isFavorite = classname === 'fa fa-heart' ? true : false,
+                oItems = oItemList.getElementsByClassName('item'),
+                index = Array.prototype.indexOf.call(oItems, oCurLi),
+                url = './history',
+                req = {
+                    user_id: userId,
+                    favorite: itemArr[index]
+                };
+            var method = !isFavorite ? 'POST' : 'DELETE';
+
+            ajax({
+                method: method,
+                url: url,
+                data: req,
+                success: function (res) {
+                    if (res.status === 'OK' || res.result === 'SUCCESS') {
+                        tar.className = !isFavorite ? 'fa fa-heart' : 'fa fa-heart-o';
+                    } else {
+                        throw new Error('Change Favorite failed!')
+                    }
+                },
+                error: function () {
+                    throw new Error('Change Favorite failed!')
+                }
+            })
+        }
+    }
+
+    /**
+     * API Register
+     */
     function registerExecutor() {
-        console.log('register');
         var username = oRegisterUsername.value,
             password = oRegisterPwd.value,
             firstName = oRegisterFirstName.value,
             lastName = oRegisterLastName.value;
-        console.log(username, password,firstName, lastName);
 
-        if (username === "" || password === "" || firstName === ""
+        if (username === "" || password == "" || firstName === ""
             || lastName === "") {
             oRegisterResultField.innerHTML = 'Please fill in all fields';
             return;
@@ -89,40 +174,219 @@
             return;
         }
         password = md5(username + md5(password));
+
+        ajax({
+            method: 'POST',
+            url: './register',
+            data: {
+                user_id : username,
+                password : password,
+                first_name : firstName,
+                last_name : lastName,
+            },
+            success: function (res) {
+                if (res.status === 'OK' || res.result === 'OK') {
+                    oRegisterResultField.innerHTML = 'Successfully registered!'
+                } else {
+                    oRegisterResultField.innerHTML = 'User already existed!'
+                }
+            },
+            error: function () {
+                //show login error
+                throw new Error('Failed to register');
+            }
+        })
     }
 
-    function switchLoginRegister(name) {
-        // hide header elements
-        showOrHideElement(oAvatar, 'none');
-        showOrHideElement(oWelcomeMsg, 'none');
-        showOrHideElement(oLogoutBtn, 'none');
+    /**
+     * API Load Nearby Items
+     */
+    function loadNearbyData() {
+        // active side bar buttons
+        activeBtn('nearby-btn');
 
-        // hide item list area
-        showOrHideElement(oItemNav, 'none');
-        showOrHideElement(oItemList, 'none');
+        var opt = {
+            method: 'GET',
+            url: './search?user_id=' + userId + '&lat=' + lat + '&lon=' + lng,
+            data: null,
+            message: 'nearby'
+        }
+        serverExecutor(opt);
+    }
 
-        if(name === 'login') {
-            // hide register form
-            showOrHideElement(oRegisterForm, 'none');
-            // clear register error
-            oRegisterResultField.innerHTML = ''
+    /**
+     * API Load Favorite Items
+     */
+    function loadFavoriteItems() {
+        activeBtn('fav-btn');
+        var opt = {
+            method: 'GET',
+            url: './history?user_id=' + userId,
+            data: null,
+            message: 'favorite'
+        }
+        serverExecutor(opt);
+    }
 
-            // show login form
-            showOrHideElement(oLoginForm, 'block');
+    /**
+     * API Load Recommended Items
+     */
+    function loadRecommendedItems() {
+        activeBtn('recommend-btn');
+        var opt = {
+            method: 'GET',
+            url: './recommendation?user_id=' + userId + '&lat=' + lat + '&lon=' + lng,
+            data: null,
+            message: 'recommended'
+        }
+        serverExecutor(opt);
+    }
 
+    /**
+     * Render Data
+     * @param data
+     */
+    function render(data) {
+        var len = data.length,
+            list = '',
+            item;
+        for (var i = 0; i < len; i++) {
+            item = data[i];
+            list += oTpl.replace(/{{(.*?)}}/gmi, function (node, key) {
+                console.log(key)
+                if(key === 'company_logo') {
+                    return item[key] || 'https://via.placeholder.com/100';
+                }
+                if (key === 'location') {
+                    return item[key].replace(/,/g, '<br/>').replace(/\"/g, '');
+                }
+                if (key === 'favorite') {
+                    return item[key] ? "fa fa-heart" : "fa fa-heart-o";
+                }
+                return item[key];
+            })
+        }
+        oItemList.innerHTML = list;
+    }
+
+    function activeBtn(btnId) {
+        var len = oNavBtnList.length;
+        for (var i = 0; i < len; i++) {
+            oNavBtnList[i].className = 'main-nav-btn';
+        }
+        var btn = document.getElementById(btnId);
+        btn.className += ' active';
+    }
+
+    /**
+     * Fetch Geolocation
+     * @param cb
+     */
+    function initGeo(cb) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                    // lat = position.coords.latitude || lat;
+                    // lng = position.coords.longitude || lng;
+                    cb();
+                },
+                function () {
+                    throw new Error('Geo location fetch failed!!')
+                }, {
+                    maximumAge: 60000
+                });
+            oItemList.innerHTML = '<p class="notice"><i class="fa fa-spinner fa-spin"></i>Retrieving your location...</p>';
         } else {
-            // hide login form
-            showOrHideElement(oLoginForm, 'none');
-            // clear login error if existed
-            oLoginErrorField.innerHTML = '';
-
-            // show register form
-            showOrHideElement(oRegisterForm, 'block');
+            throw new Error('Your browser does not support navigator!!')
         }
     }
 
     function showOrHideElement(ele, style) {
         ele.style.display = style;
+    }
+
+    function welcomeMsg(info) {
+        userId = info.user_id || userId;
+        userFullName = info.name || userFullName;
+        oWelcomeMsg.innerHTML = 'Welcome ' + userFullName;
+
+        // show welcome, avatar, item area, logout btn
+        showOrHideElement(oWelcomeMsg, 'block');
+        showOrHideElement(oAvatar, 'block');
+        showOrHideElement(oItemNav, 'block');
+        showOrHideElement(oItemList, 'block');
+        showOrHideElement(oLogoutBtn, 'block');
+
+        // hide login form
+        showOrHideElement(oLoginForm, 'none');
+    }
+
+    function fetchData() {
+        // get geo-location info
+        initGeo(loadNearbyData);
+    }
+
+    /**
+     * Helper - AJAX
+     * @param opt
+     */
+    function ajax(opt) {
+        var opt = opt || {},
+            method = (opt.method || 'GET').toUpperCase(),
+            url = opt.url,
+            data = opt.data || null,
+            success = opt.success || function () {
+            },
+            error = opt.error || function () {
+            },
+            xhr = new XMLHttpRequest();
+
+        if (!url) {
+            throw new Error('missing url');
+        }
+
+        xhr.open(method, url, true);
+
+        if (!data) {
+            xhr.send();
+        } else {
+            xhr.setRequestHeader('Content-type', 'application/json;charset=utf-8');
+            xhr.send(JSON.stringify(data));
+        }
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                success(JSON.parse(xhr.responseText))
+            } else {
+                error()
+            }
+        }
+
+        xhr.onerror = function () {
+            throw new Error('The request could not be completed.')
+        }
+    }
+
+    /**
+     * Helper - Get Data from Server
+     */
+    function serverExecutor(opt) {
+        oItemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-triangle"></i>Loading ' + opt.message + ' item...</p>';
+        ajax({
+            method: opt.method,
+            url: opt.url,
+            data: opt.data,
+            success: function (res) {
+                if (!res || res.length === 0) {
+                    oItemList.innerHTML = '<p class="notice"><i class="fa fa-exclamation-triangle"></i>No ' + opt.message + ' item!</p>';
+                } else {
+                    render(res);
+                    itemArr = res;
+                }
+            },
+            error: function () {
+                throw new Error('No ' + opt.message + ' items!');
+            }
+        })
     }
 
     init();
