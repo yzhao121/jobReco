5ecfe80aa997ad7ebd23e01191aacac5388c82fe
yzhao121 <50305405+yzhao121@users.noreply.github.com>
2021-02-20 19:11:36 -0500

    Update MySQLDBUtil.java

--- src/main/java/com/ita/job/db/MySQLDBUtil.java
+++ src/main/java/com/ita/job/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "jobrecmd";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "360291736";
+    private static final String PASSWORD = "*********";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" +DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
